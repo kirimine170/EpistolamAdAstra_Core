fcd0f99aec8e6f7b08086c5d613a972cfc6e695c
kirimine170 <kirimine170@gmail.com>
2023-11-15 11:43:40 +0900

    ✏️ update messages_controller.rb
    CURLで操作できるようにした。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,32 @@
 class MessagesController < ApplicationController
+    before_action :set_message, only: %i[ destroy ]
 
+    # GET /messages
     def index
         @messages = Message.order("RANDOM()").limit(1)
+        render status:200, json: @messages
     end
 
-    def new
-        @message = Message.new
-    end
-
+    # POST /messages
     def create
         @message = Message.new(message_params)
-        
+
         if @message.save
-            redirect_to messages_path
+            render json: @message, status: :created, location: @message
         else
-            render :new
+            render json: @message.errors, status: :unprocessable_entity
         end
     end
 
+    # DELETE /messages/1
+    def destroy
+        @message.destroy!
+    end
+
     private
+    def set_message
+        @message = Message.find(params[:id])
+    end
     def message_params
         params.require(:message).permit(:dear, :content)
     end
