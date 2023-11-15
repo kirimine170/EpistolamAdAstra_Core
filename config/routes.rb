cd1a0d093a7fc6faa7be3284ade616e5cf572a6c
kirimine170 <kirimine170@gmail.com>
2023-11-15 11:42:49 +0900

    ✏️ update routes.rb
    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   
-  resources :messages, only: [:index, :new, :create]
-  root "top#index"
+  resources :messages, only: [:index, :create, :destroy]
 end
